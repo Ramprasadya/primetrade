4101260a581c3773c30bd6a56f51143f3cf9cea0
Ramprasad yadav <yadavramprasad563@gmail.com>
2026-01-01 11:35:23 +0530

    refactor CORS configuration to specify allowed origins and improve error handling

--- server/server.js
+++ server/server.js
@@ -12,7 +12,28 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://primetrade-ilzk.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+    
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
