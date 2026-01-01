59d8a827a068af48d04a92330c3a80cdacb0d98f
Ramprasad yadav <yadavramprasad563@gmail.com>
2026-01-01 11:40:10 +0530

    refactor CORS configuration and add Vercel deployment settings

--- server/server.js
+++ server/server.js
@@ -12,28 +12,7 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://primetrade-ilzk.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-    
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
